6ef2a4c11f90c30862b13c0305d4f47e2c279743
Nils <nils.peters@kpnmail.nl>
2018-06-19 12:32:08 +0200

    bar charts added

--- dataproj.js
+++ dataproj.js
@@ -1,9 +1,8 @@
-// elements
-var $page = $(".page");
+var $page = $('.page');
 
-$(".menu_toggle").on("click", function() {
-  $page.toggleClass("shazam");
+$('.menu_toggle').on('click', function(){
+  $page.toggleClass('shazam');
 });
-$(".content").on("click", function() {
-  $page.removeClass("shazam");
+$('.content').on('click', function(){
+  $page.removeClass('shazam');
 });
